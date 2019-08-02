d5bd4ac43e0d655e69ab852968cc2d03217fb7e4
gdaly <daly.ghaith@gmail.com>
2019-08-02 19:58:08 +0100

    Adding pagination for category products list endpoint [ GET /api/v1/categories/<id>/products ]

--- src/Action/Category/CategoryProductsList.php
+++ src/Action/Category/CategoryProductsList.php
@@ -4,8 +4,12 @@ namespace App\Action\Category;
 
 use App\Action\BaseAction;
 use App\Entity\Category;
+use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\View\View;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\Pagerfanta;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Swagger\Annotations as SWG;
@@ -34,22 +38,49 @@ class CategoryProductsList extends BaseAction
      *
      * @Rest\Get("/{id}/products")
      *
+     * @SWG\Parameter(
+     *     name="page",
+     *     in="query",
+     *     type="integer",
+     *     required=false,
+     * )
      * @SWG\Response(response=200, description="Category products list get success")
-     * @SWG\Response(response=404, description="App\\Entity\\Category object not found by the @ParamConverter annotation.")
+     * @SWG\Response(response=404, description="App\\Entity\\Category object not found by the @ParamConverter annotation. / Page Not Found")
      *
      * @SWG\Tag(name="Categories")
      *
      * @Rest\View(serializerGroups={"list"})
+     * @param Request $request
      * @param Category $category
      * @return View
      */
-    public function __invoke(Category $category)
+    public function __invoke(Request $request, Category $category)
     {
+        $page = $request->query->get('page', 1);
+
+        $qb = $this->em
+            ->getRepository(Product::class)
+            ->findByCategoryQueryBuilder($category);
+
+        $adapter = new DoctrineORMAdapter($qb);
+        $pagerfanta = new Pagerfanta($adapter);
+
+        $pagerfanta->setMaxPerPage(10);
+        $pagerfanta->setCurrentPage($page);
+
+        $products = array();
+
+        foreach ($pagerfanta->getCurrentPageResults() as $product){
+            $products [] = $product;
+        }
+
         return $this->jsonResponse(
             Response::HTTP_OK,
             'Category products list get success',
             [
-                'products' => $category->getProducts()
+                'total' => $pagerfanta->getNbResults(),
+                'count' => count($products),
+                'products' => $products
             ]
         );
     }
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Category;
 use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
@@ -19,32 +20,13 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
-    // /**
-    //  * @return Product[] Returns an array of Product objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public function findByCategoryQueryBuilder(Category $category)
     {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+        $query = $this->createQueryBuilder('product')
+            ->innerJoin('product.categories', 'category')
+            ->where('category = :category')
+            ->setParameter('category', $category);
 
-    /*
-    public function findOneBySomeField($value): ?Product
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        return $query;
     }
-    */
 }
